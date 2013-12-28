574ced80c669232e319ef7b4e3833ef734f93d59
Two <sfeldbin@googlemail.com>
2013-12-28 20:01:09 +0100

    - Updated version number

--- two/newdawn/NewDawn.java
+++ two/newdawn/NewDawn.java
@@ -15,7 +15,7 @@ import two.newdawn.worldgen.biomes.VanillaBiomeProvider;
  *
  * @author Two
  */
-@Mod(modid = "newdawn", name = "New Dawn", version = "131228")
+@Mod(modid = "newdawn", name = "New Dawn", version = "131229")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
 public class NewDawn {
 
